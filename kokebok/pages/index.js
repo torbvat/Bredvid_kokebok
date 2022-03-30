2f315ec69f69ec8e9572799747612132dcbc6cf3
torbvat <torbvat@stud.ntnu.no>
2022-03-30 19:27:42 +0200

    ymse

--- kokebok/pages/index.js
+++ kokebok/pages/index.js
@@ -1,17 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-// export const Home2 = () => 
-//   <div>
-//     <h3>Under-underoverskrift</h3>
-//     <h4>Under-under-underoverskrift</h4>
-//   </div>
+
+
+ const message3 = () => {
+   return(
+  <p>tekst3</p>
+   )
+ }
+
+function message(){
+  return(
+    <p>Tekst</p>
+  )
+}
+let message2 = <p>tekst2</p>
 
 export default function Home() {
-  return (
-    <>
-    <h1>Overskrift</h1>
-    <h2>Underoverskrift</h2>
-    </>
+  return(
+    <div>
+      <h1>Overskrift</h1>
+      <h2>Underoverskrift</h2>
+      <h3>Under-underoverskrift</h3>
+      <h4>Under-under-underoverskrift</h4>
+      {message()}
+      {message2}
+      {message3()}
+    </div>
   )
 }
