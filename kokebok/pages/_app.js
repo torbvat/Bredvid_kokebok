4a2c5ecd7bbaf38fbca4abce0787bfcc888d13d4
torbvat <torbvat@stud.ntnu.no>
2022-03-30 18:56:09 +0200

    Dette endringen vil endre og legge til litt at hvert

---
+++ kokebok/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component className="app" {...pageProps} />
+}
+
+export default MyApp
+
---
+++ kokebok/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+// export const Home2 = () => 
+//   <div>
+//     <h3>Under-underoverskrift</h3>
+//     <h4>Under-under-underoverskrift</h4>
+//   </div>
+
+export default function Home() {
+  return (
+    <>
+    <h1>Overskrift</h1>
+    <h2>Underoverskrift</h2>
+    </>
+  )
+}
